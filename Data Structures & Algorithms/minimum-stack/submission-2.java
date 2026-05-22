6423319125386436ab9975e5a8f131bf7deac308
Shubh Gupta <71187757+shubhgupta2005@users.noreply.github.com>
2026-05-22 15:53:00 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,37 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer>minstack;
+
+    public MinStack() {
+        stack=new Stack<>();
+        minstack=new Stack<>();
+
+        
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minstack.isEmpty() || minstack.peek()>=val){
+            minstack.push(val);
+        }
+        
+    }
+    
+    public void pop() {
+        if(stack.isEmpty())return;
+        int top =stack.pop();
+        if(top==minstack.peek()){
+            minstack.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
