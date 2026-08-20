14a6a37647224da3fbf5598ba15ae6ac17976260
Shubh Gupta <71187757+shubhgupta2005@users.noreply.github.com>
2026-08-20 15:59:42 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        char[] ch=s.toCharArray();
+        int l=0;
+        int r=ch.length-1;
+        int count=0;
+
+        while(l<r){
+            if(Character.toLowerCase(ch[l])!=Character.toLowerCase(ch[r])){
+                if(count<2){
+                    r--;
+                    count++;
+                    if(Character.toLowerCase(ch[l])!=Character.toLowerCase(ch[r])) return false;
+
+                }
+            }else{
+                l++;
+                r--;
+            }
+        }
+        return true;
+        
+    }
+}
